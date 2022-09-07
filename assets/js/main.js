d2398cc11efc1cedc6b5720f81c8dad59ddc296d
ali-dev-code <ali.asgr514@gmail.com>
2022-09-07 15:41:49 +0500

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+(function () {
+
+	window.onload = function () {
+		window.setTimeout(fadeout, 0);
+	};
+  function fadeout() {
+		document.querySelector(".page-loader").style.opacity = "0";
+		document.querySelector(".page-loader").style.display = "none";
+	}
+
+
+ // WOW active
+ new WOW().init();
+
+
+})();
